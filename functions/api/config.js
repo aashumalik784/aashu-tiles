088cfa0fe4ce8f20b925068efbac73cdcbc092c8
aashumalik784 <aashumalik784@gmail.com>
2026-05-21 01:08:26 +0530

    Delete functions/api directory

--- functions/api/config.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet(context) {
-  return new Response(context.env.FIREBASE_CONFIG, {
-    headers: { 
-      'Content-Type': 'application/json',
-      'Cache-Control': 'public, max-age=3600' 
-    }
-  });
-}
