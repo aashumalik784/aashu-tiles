efa99511d44e7d8c8fa2ee97861aac43749efa84
aashumalik784 <aashumalik784@gmail.com>
2026-05-21 22:56:42 +0530

    contact.js

--- functions/contact.js
+++ functions/contact.js
@@ -1,1 +1,11 @@
+export async function onRequestPost(context) {
+  const formData = await context.request.formData();
+  const name = formData.get("name");
+  const phone = formData.get("phone");
+  const message = formData.get("message");
 
+  return new Response(
+    JSON.stringify({ ok: true, name, phone, message }),
+    { headers: { "Content-Type": "application/json" } }
+  );
+}
